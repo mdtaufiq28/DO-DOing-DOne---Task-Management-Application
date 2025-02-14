9d379837655c4fa26ea54b5b74abbe7cafde135b
mdtaufiq28 <mohammed.taufiq.n@gmail.com>
2025-02-14 08:15:25 +0530

    made the project production build again

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Suspense, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css';
 const LazyIntroPage=React.lazy(()=>{
   return import('./components/IntroPage')
